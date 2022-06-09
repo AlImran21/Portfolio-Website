0410e11eb430c4853d56c9189627f17e37994672
Al-Imran <alimran21feb@gmail.com>
2022-06-10 03:55:51 +0600

    added footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import './Footer.css';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>THis is footer page</h1>
-        </div>
+        <footer>
+            <a href="#" className='footer_logo'>Al-Imran</a>
+
+            <ul className='permalinks'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className='footer_socials'>
+                <a href="https://facebook.com" target="_blank"><FaFacebookF /></a>
+                <a href="https://instagram.com" target="_blank"><FiInstagram /></a>
+                <a href="https://twitter.com" target="_blank"><FaTwitter /></a>
+            </div>
+
+            <div className='footer_copyright'>
+                <small>&copy; Al-Imran. All rights reserved</small>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,9 +3,9 @@ import './Portfolio.css';
 import IMG1 from '../../assets/HAMMER.BD-screenshot.png';
 import IMG2 from '../../assets/Fruits Warehouse-screenshot.png';
 import IMG3 from '../../assets/Wild Photographer-screenshot.png';
-import IMG4 from '../../assets/portfolio1.jpg';
-import IMG5 from '../../assets/portfolio2.jpg';
-import IMG6 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/book reviewed-screenshot.JPG';
+import IMG5 from '../../assets/guitar store-screenshot.JPG';
+import IMG6 from '../../assets/convention center-screenshot.JPG';
 
 
 const data = [
@@ -14,43 +14,43 @@ const data = [
         id: 1,
         image: IMG1,
         title: 'HAMMER.BD',
-        github: 'https://github.com/AlImran21',
+        github: 'https://github.com/AlImran21/HAMMER.BD-Client',
         demo: 'https://my-assignment-12-f75b8.web.app/'
     },
     {
         id: 2,
         image: IMG2,
-        title: 'HAMMER.BD',
-        github: 'https://github.com/AlImran21',
-        demo: 'https://my-assignment-12-f75b8.web.app/'
+        title: 'Fruits Warehouse',
+        github: 'https://github.com/AlImran21/Fruits-Warehouse-Client',
+        demo: 'https://my-assignment-11-with-node.web.app/'
     },
     {
         id: 3,
         image: IMG3,
-        title: 'HAMMER.BD',
-        github: 'https://github.com/AlImran21',
-        demo: 'https://my-assignment-12-f75b8.web.app/'
+        title: 'Wild Photographer',
+        github: 'https://github.com/AlImran21/Wild-Photographer',
+        demo: 'https://my-assignment10-with-react.web.app/'
     },
     {
         id: 4,
         image: IMG4,
-        title: 'HAMMER.BD',
-        github: 'https://github.com/AlImran21',
-        demo: 'https://my-assignment-12-f75b8.web.app/'
+        title: 'A Book Reviewed',
+        github: 'https://github.com/AlImran21/Product-Analysis',
+        demo: 'https://my-assignment-9-with-react-router.netlify.app/'
     },
     {
         id: 5,
         image: IMG5,
-        title: 'HAMMER.BD',
-        github: 'https://github.com/AlImran21',
-        demo: 'https://my-assignment-12-f75b8.web.app/'
+        title: 'Guitar Store',
+        github: 'https://github.com/AlImran21/Lucky-One',
+        demo: 'https://my-assignment8-with-react.netlify.app/'
     },
     {
         id: 6,
         image: IMG6,
-        title: 'HAMMER.BD',
-        github: 'https://github.com/AlImran21',
-        demo: 'https://my-assignment-12-f75b8.web.app/'
+        title: 'Convention Center',
+        github: 'https://github.com/AlImran21/Convention-Center',
+        demo: 'https://my-third-assignment-project.netlify.app/'
     }
 
 ]
